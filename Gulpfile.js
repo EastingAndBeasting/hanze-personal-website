7c6f628e20cbf97b43d9e2bc45c7ed5182a73301
Mark <mark@wildsea.nl>
2016-10-29 19:46:17 +0200

    Added gulp watch and error handling

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,10 @@ var data = require('gulp-data');
 var htmlbeautify = require('gulp-html-beautify');
 
 gulp.task('default', function () {
+	gulp.watch('src/*.+(html|nunjucks)',['compile']);
+});
+
+gulp.task('compile', function() {
   return gulp.src('src/*.+(html|nunjucks)')
     .pipe(data(function() {
       return require('./src/data/data.json')
@@ -11,8 +15,12 @@ gulp.task('default', function () {
     .pipe(nunjucksRender({
       path: 'src/partials/'
     }))
+    .on('error', function(e) {
+      console.log(e);
+      this.emit('end');
+    })
     .pipe(htmlbeautify({
       indent_size: 4
     }))
-    .pipe(gulp.dest('./app'))
+    .pipe(gulp.dest('./app'));
 });
